551766b41de9e36d653f1c2fb5a86678b8d99d57
Andriy Fetsyuk <AndreusAnd@gmail.com>
2017-12-10 14:11:52 +0200

    cleanup

--- static/src/Routes.jsx
+++ static/src/Routes.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, IndexRedirect, IndexRoute, withRouter, Redirect } from 'react-router';
+import { Route, withRouter } from 'react-router';
 
 import App from './App.jsx';
 import IssueList from './IssueList.jsx';
