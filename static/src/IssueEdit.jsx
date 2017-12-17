cb3b24042c58b05e0ed95afa7d9c17bf4fbafde6
Andriy F <AndreusAnd@gmail.com>
2017-12-17 14:31:10 +0200

    Fixed date on issue edit form

--- static/src/IssueEdit.jsx
+++ static/src/IssueEdit.jsx
@@ -220,7 +220,7 @@ dismissToast(){
           <FormGroup validationState={this.state.invalidFields.completionDate ? 'error' : null}>
             <Col componentClass={ControlLabel} sm={3}> Completion Date </Col>
             <Col sm={9}>
-              <FormControl componentClass={DateInput} name="completionDate"  value={this.state.issue.completionDate}
+              <FormControl componentClass={DateInput} name="completionDate"  value={this.state.issue.completionDate && this.state.issue.completionDate.toString()}
                 onChange={this.onChange} onValidityChange={this.onValidityChange} />
                 <FormControl.Feedback />
             </Col>
